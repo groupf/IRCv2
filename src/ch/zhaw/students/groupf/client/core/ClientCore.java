2f1bca5abcf6abedb2050481cfe3aabe9a02b800
esterren <renato.estermann@gmail.com>
2011-10-06 20:51:04 +0200

    added doc Folder

--- src/ch/zhaw/students/groupf/client/core/ClientCore.java
+++ src/ch/zhaw/students/groupf/client/core/ClientCore.java
@@ -1,5 +1,11 @@
 package ch.zhaw.students.groupf.client.core;
 
-public class ClientCore {
+public class ClientCore implements IClientCore {
+
+	@Override
+	public void sendMsg(String message) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
